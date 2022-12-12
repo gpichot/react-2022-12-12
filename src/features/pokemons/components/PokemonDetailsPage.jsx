7090c85ef74514e352105c3f7ae11b39c687a790
Gabriel Pichot <gabriel.pichot@gmail.com>
2022-12-12 17:37:12 +0100

    feat: correction useFetchResource

--- src/features/pokemons/components/PokemonDetailsPage.jsx
+++ src/features/pokemons/components/PokemonDetailsPage.jsx
@@ -1,11 +1,53 @@
 import { useParams } from "react-router-dom";
 
+import useFetchResource from "@/hooks/useFetchResource";
+
+import PokemonTypePill from "./PokemonTypePill";
+
 export default function PokemonDetailsPage() {
   const params = useParams();
-  console.log(params);
+  const { id } = params;
+
+  const pokemonDetail = useFetchResource(
+    `https://pokeapi.fly.dev/gpichot20221212/pokemons/${id}`
+  );
+
+  if (pokemonDetail.isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (pokemonDetail.error) {
+    return <p>Something went wrong</p>;
+  }
+
+  const pokemon = pokemonDetail.data;
+
   return (
     <>
-      <h1>Details for pokemon with id {params.id}</h1>
+      <h1>{pokemon.name}</h1>
+      <div>
+        <img src={pokemon.image} alt={pokemon.name} />
+      </div>
+
+      {pokemon.types.map((type) => (
+        <PokemonTypePill key={type} type={type} />
+      ))}
+
+      <h2>Abilities</h2>
+      <ul>
+        {pokemon.abilities.map((ability) => (
+          <li key={ability}>{ability}</li>
+        ))}
+      </ul>
+
+      <h2>Stats</h2>
+      <ul>
+        {Object.entries(pokemon.stats).map(([statName, statValue]) => (
+          <li key={statName}>
+            {statName}: {statValue}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
